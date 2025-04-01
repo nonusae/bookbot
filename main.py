a0524d48768c8239e8c36ab5b934400c849a1db4
Natthakit Iewprasert <nonusae@gmail.com>
2025-04-01 13:54:31 +0700

    Update main.py to read text file from path

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("greetings boots")
+def get_book_text(path):
+    with open(path, "r") as file:
+        return file.read()
+
+def main():
+    print(get_book_text("./books/frankenstein.txt"))
+
+if __name__ == "__main__":
+    main()
